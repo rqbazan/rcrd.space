4521a33a6c75099a1dd0e8293be9848f9e81be1b
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-09-17 18:40:02 -0500

    chore: update links for static content

--- src/components/burger/index.js
+++ src/components/burger/index.js
@@ -4,13 +4,13 @@ export default function Burger(props) {
   return (
     <div role="button" onClick={onClick} onKeyPress={onClick} tabIndex={0}>
       <img
-        src="static/colorful-burger.svg"
+        src="/static/colorful-burger.svg"
         alt="burger"
         height={32}
         style={{ display: isColorful ? 'block' : 'none' }}
       />
       <img
-        src="static/grayscale-burger.svg"
+        src="/static/grayscale-burger.svg"
         alt="burger"
         height={32}
         style={{ display: isColorful ? 'none' : 'block' }}
