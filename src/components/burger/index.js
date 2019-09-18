e96c09c7fabfcf7ae723c062d5785e202b535276
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-09-17 19:15:11 -0500

    fix: remove svg burgers

--- src/components/burger/index.js
+++ src/components/burger/index.js
@@ -4,13 +4,13 @@ export default function Burger(props) {
   return (
     <div role="button" onClick={onClick} onKeyPress={onClick} tabIndex={0}>
       <img
-        src="/static/colorful-burger.svg"
+        src="/static/colorful-burger.png"
         alt="burger"
         height={32}
         style={{ display: isColorful ? 'block' : 'none' }}
       />
       <img
-        src="/static/grayscale-burger.svg"
+        src="/static/grayscale-burger.png"
         alt="burger"
         height={32}
         style={{ display: isColorful ? 'none' : 'block' }}
