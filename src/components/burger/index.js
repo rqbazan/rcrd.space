b31c2a25b6aad72263d940ee503ad98776afebc1
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-09-27 22:19:50 -0500

    fix: use 1:1 aspect ratio of original burger

--- src/components/burger/index.js
+++ src/components/burger/index.js
@@ -9,12 +9,14 @@ export default function Burger(props) {
         src="https://res.cloudinary.com/dpwoyjb1f/image/upload/q_auto/me/colorful-burger.png"
         alt="burger"
         height={32}
+        width={32}
         style={{ display: isColorful ? 'block' : 'none' }}
       />
       <img
         src="https://res.cloudinary.com/dpwoyjb1f/image/upload/q_auto/me/grayscale-burger.png"
         alt="burger"
         height={32}
+        width={32}
         style={{ display: isColorful ? 'none' : 'block' }}
       />
     </div>
