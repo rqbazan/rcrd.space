7dba87cfda2f9bad0e2c9d7c691bc353f3484cb7
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-09-28 17:37:14 -0500

    refactor: move remarkTwemoji function to /works lambda

--- pages/api/works.js
+++ pages/api/works.js
@@ -1,5 +1,17 @@
-import remarkTwemoji from 'src/remark-twemoji'
+import { mutateSource } from 'gatsby-remark-twemoji-shortcut'
 import data from 'src/data.json'
+
+function remarkTwemoji(markdown) {
+  const markdownNode = {
+    internal: {
+      content: markdown.slice()
+    }
+  }
+
+  mutateSource({ markdownNode })
+
+  return markdownNode.internal.content
+}
 
 export default (_, res) => {
   const workPosts = data.workPosts.map(workPost => {
