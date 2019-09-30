855ab1fb577dec01ccf39efc6a1e7270e37cdcc0
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-09-30 02:58:05 -0500

    feat: add custom config for next-offline

--- next.config.js
+++ next.config.js
@@ -7,6 +7,27 @@ module.exports = withBundleAnalyzer(
   withOffline({
     env: {
       API: process.env.API
+    },
+    transformManifest: manifest => ['/'].concat(manifest),
+    workboxOpts: {
+      swDest: 'static/service-worker.js',
+      runtimeCaching: [
+        {
+          urlPattern: /^https?.*/,
+          handler: 'NetworkFirst',
+          options: {
+            cacheName: 'https-calls',
+            networkTimeoutSeconds: 15,
+            expiration: {
+              maxEntries: 150,
+              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
     }
   })
 )
