6161d8d83bf4373b68b3d5300648b10ca4bf8507
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-10-01 13:48:45 -0500

    feat: remove workbox support

--- next.config.js
+++ next.config.js
@@ -1,32 +1,5 @@
-const withOffline = require('next-offline')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer(
-  withOffline({
-    env: {
-      API: process.env.API
-    },
-    transformManifest: manifest => ['/'].concat(manifest),
-    workboxOpts: {
-      swDest: 'static/service-worker.js',
-      runtimeCaching: [
-        {
-          urlPattern: /\.(?:png|jpg|jpeg|svg|gif|ico)$/,
-          handler: 'CacheFirst',
-          options: {
-            cacheName: 'images',
-            expiration: {
-              maxEntries: 150,
-              maxAgeSeconds: 30 * 24 * 60 * 60 // 1 month
-            },
-            cacheableResponse: {
-              statuses: [0, 200]
-            }
-          }
-        }
-      ]
-    }
-  })
-)
+module.exports = withBundleAnalyzer()
