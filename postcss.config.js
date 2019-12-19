380953a99d80cfa25c6bbdd861eeef603e4da665
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-19 17:30:49 -0500

    chore: add tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,22 @@
+/* eslint global-require: "off" */
+const PROD = process.env.NODE_ENV === 'production'
+
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.js'],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
+const env = require('postcss-preset-env')({
+  autoprefixer: { grid: true },
+  features: {
+    'nesting-rules': true
+  },
+  browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+})
+
+const tw = require('tailwindcss')('./tailwind.config.js')
+
+// preset-env config from https://bit.ly/2Pnlfg0
+module.exports = {
+  plugins: [tw, env, PROD && purgecss].filter(Boolean)
+}
