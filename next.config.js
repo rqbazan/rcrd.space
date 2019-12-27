11f08330367000eb111a1855e2ae123cc5d19767
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2019-12-26 20:27:02 -0500

    feat: use next export command

--- next.config.js
+++ next.config.js
@@ -4,4 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withCSS(withBundleAnalyzer())
+module.exports = withCSS(
+  withBundleAnalyzer({
+    exportTrailingSlash: true
+  })
+)
