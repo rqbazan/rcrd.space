132f476eadcbdadd967ab073513a5b2b9e6680c4
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-06 23:27:03 -0500

    perf: minify script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,7 @@
 /* eslint react/no-danger: off */
-
 import React from 'react'
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import * as scripts from '~/scripts'
 
 class Document extends NextDocument {
   static getInitialProps = NextDocument.getInitialProps
@@ -16,20 +16,11 @@ class Document extends NextDocument {
         </body>
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              WebFontConfig = {
-                google: {
-                  families: ['IBM+Plex+Mono:400,500', 'Solway:300&display=swap']
-                }
-              };
-
-              (function(d) {
-                  var wf = d.createElement('script'), s = d.scripts[0];
-                  wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js';
-                  wf.async = true;
-                  s.parentNode.insertBefore(wf, s);
-              })(document);
-            `
+            __html: scripts.webfont({
+              google: {
+                families: ['IBM+Plex+Mono:400,500', 'Solway:300&display=swap']
+              }
+            })
           }}
         />
       </Html>
---
+++ src/scripts.js
@@ -0,0 +1,7 @@
+/* eslint prettier/prettier: off */
+
+export const webfont = config => {
+  return `WebFontConfig = ${JSON.stringify(
+    config
+  )};(function(d) { var wf = d.createElement('script'), s = d.scripts[0]; wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js'; wf.async = true; s.parentNode.insertBefore(wf, s); })(document);`
+}
