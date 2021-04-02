8a1d9e7215b9ad7fc112f009099da942c73ee10b
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2021-04-02 11:30:28 -0500

    feat: add rewrites and redirect to vercel.json

--- next.config.js
+++ next.config.js
@@ -6,17 +6,5 @@ module.exports = {
     })
 
     return config
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/bee.js',
-        destination: 'https://cdn.splitbee.io/sb.js'
-      },
-      {
-        source: '/_hive/:slug',
-        destination: 'https://hive.splitbee.io/:slug'
-      }
-    ]
   }
 }
