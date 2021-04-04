faaee41a859adc312aadb6234ee96f3bb7d5419c
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2021-04-03 21:30:07 -0500

    fix: duplicated up button

--- src/pages/work.js
+++ src/pages/work.js
@@ -2,7 +2,6 @@ import * as api from '~/lib/api'
 import { SEO } from '~/components/seo'
 import { MainLayout } from '~/layouts/main'
 import { TechProject } from '~/components/tech-project'
-import { UPButton } from '~/components/up-button'
 
 export default function WorkPage({ techProjects, techProfile }) {
   return (
@@ -16,9 +15,6 @@ export default function WorkPage({ techProjects, techProfile }) {
               {...techProject}
             />
           ))}
-        </div>
-        <div className="fixed bottom-0 right-0 mr-4 mb-4 lg:mr-8 lg:mb-8">
-          <UPButton />
         </div>
       </MainLayout>
     </>
