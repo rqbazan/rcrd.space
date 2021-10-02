5e4fea535ce882260f3e87410673e7c0ddd57c78
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2021-10-02 12:37:58 -0500

    feat: add motion to home

--- src/layouts/main/components/transition/index.tsx
+++ src/layouts/main/components/transition/index.tsx
@@ -7,7 +7,7 @@ export interface MainTransitionProps {
 export function MainTransition({ children }: MainTransitionProps) {
   return (
     <motion.div
-      transition={{ duration: 0.2 }}
+      transition={{ duration: 0.15 }}
       initial={{ opacity: 0, y: -16 }}
       animate={{ opacity: 1, y: 0 }}
       exit={{ opacity: 0, y: -16 }}
