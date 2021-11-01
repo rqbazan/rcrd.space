6391a25ddcfac829353bb34711ea3a6522de3f9c
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2021-10-31 20:15:58 -0500

    chore(deps): upgrade `just-map-values` (#18)

--- src/global.d.ts
+++
@@ -1,9 +0,0 @@
-// TODO: add PR to the type definition package of `just-map-values`
-module 'just-map-values' {
-  declare function mapValues<TInput extends {}, TMappedValue>(
-    item: TInput,
-    callback: (value: TInput[keyof TInput], key: keyof TInput, object: TInput) => TMappedValue
-  ): { [k in keyof TInput]: ReturnType<typeof callback> }
-
-  export = mapValues
-}
