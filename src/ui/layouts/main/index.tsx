bf88c208110414445bf0266088636aaff25326da
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2022-04-03 13:34:20 -0500

    fix: font sizes and max-w container

--- src/ui/layouts/main/index.tsx
+++ src/ui/layouts/main/index.tsx
@@ -7,7 +7,7 @@ export interface MainLayoutProps {
 
 export function MainLayout({ children }: MainLayoutProps) {
   return (
-    <main className="flex flex-col min-h-full xl:max-w-[980px] lg:max-w-[840px] md:max-w-[720px] px-6 md:px-4 py-8 md:py-24 mx-auto">
+    <main className="flex flex-col min-h-full md:max-w-[42rem] px-6 md:px-4 py-8 md:py-24 mx-auto">
       <MainHeader className="mb-6" />
       {children}
       <MainFooter className="mt-auto pt-16" />
