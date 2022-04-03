edfd6a7ff274730a3296cfcdeea58a3e17c5a64d
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2022-04-03 15:39:38 -0500

    fix: animated shared layouts

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react'
 import Typewriter from 'typewriter-effect'
 import { Anchor } from '~/ui'
-import { MainHeading } from '~/ui/layouts'
+import { MainHeading, MainTransition } from '~/ui/layouts'
 
 export function HomeView() {
   return (
-    <MainHeading
-      title={
-        <React.Fragment>
-          Hey, I’m{' '}
-          <Typewriter
-            options={{
-              strings: ['Santiago', 'Ricardo', 'Alexis'],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </React.Fragment>
-      }
-      content={
-        <React.Fragment>
-          I&rsquo;m a software developer from Trujillo - Peru. I work as Sr. Software Engineer at{' '}
-          <Anchor href="https://www.linkedin.com/company/pedidosya/mycompany">PedidosYa</Anchor>.
-          You’ve found my personal place on the internet.
-        </React.Fragment>
-      }
-    />
+    <MainTransition>
+      <MainHeading
+        title={
+          <React.Fragment>
+            Hey, I’m{' '}
+            <Typewriter
+              options={{
+                strings: ['Santiago', 'Ricardo', 'Alexis'],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </React.Fragment>
+        }
+        content={
+          <React.Fragment>
+            I&rsquo;m a software developer from Trujillo - Peru. I work as Sr. Software Engineer at{' '}
+            <Anchor href="https://www.linkedin.com/company/pedidosya/mycompany">PedidosYa</Anchor>.
+            You’ve found my personal place on the internet.
+          </React.Fragment>
+        }
+      />
+    </MainTransition>
   )
 }
