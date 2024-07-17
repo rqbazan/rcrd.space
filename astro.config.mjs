01efeb455d99858d0c554eff75763a1734397687
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-07-16 19:04:16 -0500

    feat: add vercel speed-insights

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -17,4 +18,10 @@ export default defineConfig({
       lastmod: new Date(),
     }),
   ],
+  output: "static",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
