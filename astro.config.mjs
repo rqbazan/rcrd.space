b47966f743e19acdc32069608e59d80bf856ffe9
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-07-16 19:42:26 -0500

    feat: add vercel speed-insights (#69)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -17,4 +18,13 @@ export default defineConfig({
       lastmod: new Date(),
     }),
   ],
+  // TODO change to static when astro/env is supported:
+  // The feature "astro:env getSecret" is experimental and subject to change (used by @astrojs/vercel/serverless).
+  // `output: "server"` or `output: "hybrid"` is required to use the serverless adapter.
+  output: "server",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
