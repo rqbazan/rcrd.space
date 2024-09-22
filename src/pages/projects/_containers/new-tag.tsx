e3b93e621a8d06fb935d0f751e1482c007dd6fea
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-09-22 16:29:16 -0300

    fix: new tag load component

---
+++ src/pages/projects/_containers/new-tag.tsx
@@ -0,0 +1,24 @@
+import { dateDiffInDays } from "~/utils/date-diff-in-days";
+import UINewTag from "../_components/new-tag.astro";
+
+interface NewTagProps {
+  postedAt?: string;
+}
+
+const MAX_DAYS_TO_BE_NEW = 30;
+
+function isNew(postedAt?: string) {
+  if (!postedAt) {
+    return false;
+  }
+
+  console.log("is new is evaluating", postedAt);
+
+  const diffInDays = dateDiffInDays(new Date(postedAt));
+
+  return diffInDays <= MAX_DAYS_TO_BE_NEW;
+}
+
+export function NewTag({ postedAt }: NewTagProps) {
+  return isNew(postedAt) ? <UINewTag class="ml-1 -mt-2" /> : null;
+}
