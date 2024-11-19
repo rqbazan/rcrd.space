687444f63e77088749fefee66fc647d13c7e4962
Ricardo Q. Bazan <raquirozbazan@gmail.com>
2024-11-18 21:19:18 -0500

    chore: add posthog

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -6,3 +6,15 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
+
+declare let posthog: {
+  init: (
+    token: string,
+    config: { api_host: string; person_profiles: string },
+  ) => void;
+  capture(event: string, properties?: Record<string, unknown>): void;
+};
+
+interface Window {
+  posthog: typeof posthog;
+}
